78d5ddaec5edc5133075f5a394f5481d53e223e0
Makvay <bojkov07@list.ru>
2025-11-16 16:40:50 +0800

    Пиздец

--- src/main/java/application/empl/EmplApplication.java
+++ src/main/java/application/empl/EmplApplication.java
@@ -6,12 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("application.empl.Repo") // Добавляем эту строку
-@EntityScan("application.empl.Entities")
+@EnableJpaRepositories("application.empl.Repo")
+@EntityScan("application.empl.Entities") // Исправлено на Entities (с i)
 public class EmplApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmplApplication.class, args);
     }
-
 }
