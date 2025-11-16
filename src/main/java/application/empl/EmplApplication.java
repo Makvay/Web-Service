e826c06a5cc7e2b063ee00cd2739d7ae2431ed8c
Makvay <bojkov07@list.ru>
2025-11-16 16:32:55 +0800

    fix entity

--- src/main/java/application/empl/EmplApplication.java
+++ src/main/java/application/empl/EmplApplication.java
@@ -2,8 +2,12 @@ package application.empl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("application.empl.Repo") // Добавляем эту строку
+@EntityScan("application.empl.Entites")
 public class EmplApplication {
 
     public static void main(String[] args) {
