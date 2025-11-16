71740db0d74be3d6c15bd90b8f9bf351d1768cc4
Makvay <bojkov07@list.ru>
2025-11-16 16:38:36 +0800

    Пиздец

--- src/main/java/application/empl/EmplApplication.java
+++ src/main/java/application/empl/EmplApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("application.empl.Repo") // Добавляем эту строку
-@EntityScan("application.empl.Entites")
+@EntityScan("application.empl.Entities")
 public class EmplApplication {
 
     public static void main(String[] args) {
